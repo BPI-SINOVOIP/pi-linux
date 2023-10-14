c6d4c50fab763bc69042728d40864ff9c5db9a1a
Banana Pi -BPI <lionwang@sinovoip.com.cn>
2023-10-14 11:17:25 +0800

    fix build

--- sound/soc/syna/berlin_playback.c
+++ sound/soc/syna/berlin_playback.c
@@ -654,7 +654,8 @@ extern void hdmi_port_int_enable(void);
 //static void hdmi_enable(struct work_struct *work)
 void hdmi_enable(struct work_struct *work)
 {
-	hdmi_port_int_enable();
+	//hdmi_port_int_enable();
+	return;
 }
 
 
