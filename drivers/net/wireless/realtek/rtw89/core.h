78e37081e7f90919fd3953dcd6c5d043ea19b41f
Banana Pi -BPI <lionwang@sinovoip.com.cn>
2024-01-01 21:41:19 +0800

    wireless back to org

--- drivers/net/wireless/realtek/rtw89/core.h
+++ drivers/net/wireless/realtek/rtw89/core.h
@@ -2807,8 +2807,7 @@ struct rtw89_fw_suit {
 			  GET_FW_HDR_SUBINDEX(fw_hdr))
 
 struct rtw89_fw_info {
-	const void *firmware;
-	size_t firmware_size;
+	const struct firmware *firmware;
 	struct rtw89_dev *rtwdev;
 	struct completion completion;
 	u8 h2c_seq;
